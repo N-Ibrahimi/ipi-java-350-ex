a892ea0c972cf476e0ff4ce24287b82bbb708ddd
nasim.ibrahimi <nasim.ibrahimi@FR-L4763320.groupinfra.com>
2020-03-17 21:19:10 +0100

    done

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -3,7 +3,6 @@ package com.ipiecoles.java.java350.repository;
 import java.time.LocalDate;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,56 +14,49 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.ipiecoles.java.java350.model.Employe;
 
-@ExtendWith(SpringExtension.class) //Junit 5 
+@ExtendWith(SpringExtension.class) // Junit 5
 @DataJpaTest // ou @SpringBootTest
 public class EmployeRepositoryTest {
-	private Logger LOGGER=LoggerFactory.getLogger(EmployeRepositoryTest.class);
-	
-	    @Autowired
-		static
-	    EmployeRepository employeRepository;
-	    
-	    @BeforeEach
-	    public static void setup() {
-	    	employeRepository.deleteAll();
-	    }
-	    
-	    
-		@Test
-		public void testFindByImmatNull(){
-		    //Given
-		    
-		    //When
-		    String result = employeRepository.findLastMatricule();
-		    
-		    //Then
-		    Assertions.assertThat(result).isNull(); 
+	private Logger LOGGER = LoggerFactory.getLogger(EmployeRepositoryTest.class);
+
+	@Autowired
+	static EmployeRepository employeRepository;
+
+	@BeforeEach
+	public void setup() {
+		employeRepository.deleteAll();
 	}
-		
-		
-	    @Test
-	    public void testFindByImmat(){
-	        //Given
-	    	Employe emp3 = employeRepository.save(new Employe("emp3", "emp3", "X55555", LocalDate.now().minusMonths(10), 2500.00, 3, 0.50));
-	        Employe emp = employeRepository.save(new Employe("emp1", "emp1", "T99999", LocalDate.now(), 2500.00, 3, 1.00));
-	        Employe emp2 = employeRepository.save(new Employe("emp2", "emp2", "A33333", LocalDate.now().minusMonths(8), 2500.00, 3, 1.00));
-	        
-	        //When
-	        String result = employeRepository.findLastMatricule();
-	        
-	        //Then
-	        Assertions.assertThat(result).isEqualTo("99999"); 
-	        
-	        
-	        //debug is thechnique    
-	        LOGGER.info("La test by immat a eté bien exécutée ");
-	        
-	        LOGGER.info("après la test: {}", result);// {} is for putting the message that comes after , in the method
-	    }
-	    
-	    
-	    
 
-	
+	@Test
+	public void testFindByImmatNull() {
+		// Given
+
+		// When
+		String result = employeRepository.findLastMatricule();
+
+		// Then
+		Assertions.assertThat(result).isNull();
+	}
+
+	@Test
+	public void testFindByImmat() {
+		// Given
+		Employe emp3 = employeRepository
+				.save(new Employe("emp3", "emp3", "X55555", LocalDate.now().minusMonths(10), 2500.00, 3, 0.50));
+		Employe emp = employeRepository.save(new Employe("emp1", "emp1", "T99999", LocalDate.now(), 2500.00, 3, 1.00));
+		Employe emp2 = employeRepository
+				.save(new Employe("emp2", "emp2", "A33333", LocalDate.now().minusMonths(8), 2500.00, 3, 1.00));
+
+		// When
+		String result = employeRepository.findLastMatricule();
+
+		// Then
+		Assertions.assertThat(result).isEqualTo("99999");
+
+		// debug is thechnique
+		LOGGER.info("La test by immat a eté bien exécutée ");
+
+		LOGGER.info("après la test: {}", result);// {} is for putting the message that comes after , in the method
+	}
+
 }
-
